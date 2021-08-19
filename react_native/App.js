3860d3fde970f103c62254f3e8546bc6a570be13
idcms <idcmsbeta@gmail.com>
2021-08-19 20:56:14 +0800

    the last button create for react native

--- react_native/App.js
+++ react_native/App.js
@@ -2,20 +2,28 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ListScreen from './List'
 import FormScreen from './Form'
+import ListScreen from './List'
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="List">
-        <Stack.Screen name="List" component={ListScreen} />
-        <Stack.Screen name="Form" component={FormScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="List">
+                <Stack.Screen name="Form" component={FormScreen} />
+                <Stack.Screen name="List" component={ListScreen}
+                              options={({navigation}) => ({
+                                  headerRight: () => <Button title="Create" onPress={() =>     navigation.navigate("Form",{
+                                      nameRoute: "",
+                                      ageRoute: "",
+                                      personIdRoute: ""
+                                  })} />,
+                              })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
